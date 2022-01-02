55313bffe7f97150f1226124eaf8a90eef39c1b8
Jun <87353476+sherpajun@users.noreply.github.com>
2022-01-02 21:56:29 +0900

    js update

--- main.js
+++ main.js
@@ -30,7 +30,9 @@ console.log(ch);
 
 //var, let, const 의 차이점 const와 let은 블럭 밖에서 선언 하면
 // 블럭 안에서 불러 사용 할 수 있다.
-// 그러나 var는 블럭 밖에서 선언 하면 블럭 밖에서만 사용 할 수 있다.
+// 그러나 var는 블럭 안에서 선언 하면 블럭 밖에서도 사용 할 수 있다.
 
+
+//String Number boolean null undefined
 
 
